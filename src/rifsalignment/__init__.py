3921990b0392b92de29ecd9138a70a4a3e281043
Anders Stendevad <as@flowtale.ai>
2023-05-15 19:54:31 +0200

    Bumped version

--- src/rifsalignment/__init__.py
+++ src/rifsalignment/__init__.py
@@ -12,7 +12,7 @@ from rifsalignment.algorithms import (
 )
 from rifsalignment.experiment import align_experiment_folder, check_for_good_alignment
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 alignment_methods = {
     "ctc": CTC,
